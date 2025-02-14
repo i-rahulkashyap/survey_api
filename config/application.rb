966006e97fccb962bc9f41d2c9759d82448df7d3
Rahul Kashyap <34953559+i-rahulkashyap@users.noreply.github.com>
2025-02-14 08:35:35 +0530

    refactor: deployment

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,6 @@ module SurveyApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.assets.enabled = false
 
     config.autoload_paths << Rails.root.join('app/lib')
     # Add middleware for cookies and sessions:
